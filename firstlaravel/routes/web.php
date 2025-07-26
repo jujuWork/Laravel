1056710a80c3eb55eed41dfe5905b0f18135bdb4
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-07-26 21:18:21 +0900

    web.php
    route

--- firstlaravel/routes/web.php
+++ firstlaravel/routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
 
+Route::resource('post', PostController::class);
