2d412c8f4ba47ac6e14507d905040e2f1a00fb56
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-07-28 14:56:51 +0900

    072825

--- firstlaravel/resources/views/posts/index.blade.php
+++ firstlaravel/resources/views/posts/index.blade.php
@@ -5,8 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All Posts</title>
 
-    <!-- @vite(['resources/js/app.js'])  -->
-
     <!-- Bootstrap 5 CDN -->
      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
