547504caf37d51a7e9ce33937b8b79ee96692b0e
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-07-30 10:56:26 +0900

    073025

---
+++ lara-react-crud/resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel - React</title>
+
+    <!-- React -->
+    @viteReactRefresh
+    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+</head>
+<body>
+    
+</body>
+</html>
