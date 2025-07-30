1fae67696cd60c221ee63693fe9c65be063e83af
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-07-30 10:09:04 +0900

    073025

--- lara-react-crud/resources/js/app.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-function App() {
-    return <h1>Hello from React!</h1>
-}
-
-const root = ReactDOM.createRoot(document.getElementById('app'));
-root.render(<App />);
